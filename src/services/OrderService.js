895f6571bf836bf87e7eeeb2c0c8b6bc8aaa232f
Ziqian Ge <ge.zi@husky.neu.edu>
2019-06-08 06:39:08 -0400

    Costumer side services added, need a backend for further dev

---
+++ src/services/OrderService.js
@@ -0,0 +1,48 @@
+/**
+ * This service stores information of an order that User is making or receiving.
+ * When making an order, information would be saved locally and would not be uploaded to the server.
+ * After costumer verifies the order, it would be send to the server for restaurant to read.
+ */
+class OrderService {
+
+    // 顾客入店后创建。可以本地暂存点单。此时OrderService作为伪后端。可以考虑编辑中的点单也传到后端保存
+    constructor(userId, restaurantId) {
+        this.userId = userId;
+        this.restaurantId = restaurantId;
+        this.order = new Map();
+        this.foodId = 0;
+    }
+
+    // 在Rabbit House 点单
+    addFood(food) {
+        this.order.set(this.foodId, {...food, foodId: this.foodId});
+        this.foodId++;
+    }
+
+    // 查看当前点单
+    viewCurrentOrder() {
+        return Array.from(this.order.values());
+    }
+
+    // 查看指定黑暗料理
+    viewFoodById(foodId) {
+        return this.order.get(foodId);
+    }
+
+    // 这道菜搞不好不是黑暗料理...
+    deleteFood(foodId) {
+        this.order.delete(foodId);
+    }
+
+    // 修改Rabbit House 的点单
+    updateFood(foodId, food) {
+        this.order.set(foodId, food);
+    }
+
+    // 下单
+    sendOrder() {
+
+    }
+}
+
+export default OrderService;
---
+++ src/services/RestaurantService.js
@@ -0,0 +1,45 @@
+// 商店街管理委员会
+class RestaurantService {
+
+    static instance;
+
+    static getInstance() {
+        if (!this.instance) {
+            this.instance = new RestaurantService();
+        }
+        return this.instance;
+    }
+
+    // 为了拯救非洲难民，几位猫娘决定开一家咖啡厅名为Rabbit House...
+    createRestaurant(restaurantName) {
+
+    }
+
+    // 逛街
+    findAllRestaurants() {
+
+    }
+
+    // 服务器检查Rabbit House 是否存在，若存在，返回Rabbit House 的菜单 which is an ArrayOfFoods
+    findRestaurantById(restaurantId) {
+
+    }
+
+    // 查看Rabbit House 历史点单，从服务器下载
+    findOrdersOfRestaurant() {
+
+    }
+
+    // 我Rabbit House 就算是倒闭，也不会...
+    deleteRestaurant() {
+
+    }
+
+    // Rabbit House 店面装修
+    updateRestaurant(restaurantId, restaurant) {
+
+    }
+
+}
+
+export default RestaurantService;
