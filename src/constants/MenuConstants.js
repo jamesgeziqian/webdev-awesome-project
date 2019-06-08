9a4b7bb8355bff8f5455f7df580d9778bebba324
Ziqian Ge <ge.zi@husky.neu.edu>
2019-06-08 06:46:40 -0400

    constants added for redux

---
+++ src/constants/MenuConstants.js
@@ -0,0 +1,9 @@
+export const ADD_FOOD_MENU = "ADD_FOOD_MENU";
+
+export const VIEW_MENU = "VIEW_MENU";
+
+export const VIEW_FOOD_MENU = "VIEW_FOOD_MENU";
+
+export const DELETE_FOOD_MENU = "DELETE_FOOD_MENU";
+
+export const UPDATE_FOOD_MENU = "UPDATE_FOOD_MENU";
---
+++ src/constants/OrderConstants.js
@@ -0,0 +1,11 @@
+export const ADD_FOOD_ORDER = "ADD_FOOD_ORDER";
+
+export const VIEW_ORDER = "VIEW_ORDER";
+
+export const VIEW_FOOD_ORDER = "VIEW_FOOD_ORDER";
+
+export const FIND_ORDERS = "FIND_ORDERS";
+
+export const DELETE_FOOD_ORDER = "DELETE_FOOD_ORDER";
+
+export const UPDATE_FOOD_ORDER = "UPDATE_FOOD_ORDER";
