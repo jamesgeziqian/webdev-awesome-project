693b45c9143bd423460e0675c928f53785c0f4d0
Ziqian Ge <ge.zi@husky.neu.edu>
2019-06-09 14:58:42 -0400

    YelpService modified

--- src/services/YelpService.js
+++ src/services/YelpService.js
@@ -16,27 +16,14 @@ class YelpService {
         }
     }
 
-    setCoordinates(latitude, longitude) {
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
-
-    searchAutocomplete(searchTerm) {
-        let url = `${this.autocompleteUrl}?latitude=${this.latitude}&longitude=${this.longitude}`;
-        if (typeof searchTerm !== "undefined" && searchTerm.length > 0) {
-            url = `${url}&text=${searchTerm}`;
-        }
-        return fetch(url, this.headers).then((response) => response.json());
-    }
-
     searchNearBy(searchTerm) {
-        const resultAutocomplete = this.searchAutocomplete(searchTerm);
+        const resultAutocomplete = this.searchBusiness(searchTerm);
         return resultAutocomplete.then(
             (result) => {
                 return Promise.all(
                     result["businesses"].map(
                         (business) => {
-                            return this.searchBusinessById(business["id"]);
+                            return this.searchBusinessDetails(business["id"]);
                         }
                     )
                 )
@@ -44,25 +31,30 @@ class YelpService {
         );
     }
 
-    searchLocation(location) {
-        let url = `${this.businessUrl}/search`;
-        if (typeof location !== "undefined" && location.length > 0) {
-            url = `${url}?location=${location}`;
-            return fetch(url, this.headers)
-                .then((response) => response.json());
-        }
-    }
-
-    searchCoordinates(latitude, longitude) {
+    searchCoordinates(searchTerm, latitude, longitude) {
         let url = `${this.businessUrl}/search`;
         if (typeof latitude === "number" && typeof longitude === "number") {
             url = `${url}?latitude=${latitude}&longitude=${longitude}`;
+            if (typeof searchTerm !== "undefined" && searchTerm.length > 0) {
+                url = `${url}&term=${searchTerm}`;
+            }
+
             return fetch(url, this.headers)
                 .then((response) => response.json());
         }
+        return null;
     }
 
-    searchBusinessById(id) {
+    searchBusiness(searchTerm, location = "Boston") {
+        // navigator.geolocation.getCurrentPosition((location) => console.log(location));
+        let url = `${this.businessUrl}/search?location=${location}&categories=restaurant`;
+        if (typeof searchTerm !== "undefined" && searchTerm.length > 0) {
+            url = `${url}&term=${searchTerm}`;
+        }
+        return fetch(url, this.headers).then((response) => response.json());
+    }
+
+    searchBusinessDetails(id) {
         if (typeof id !== "undefined" && id.length > 0) {
             const url = `${this.businessUrl}/${id}`;
             return fetch(url, this.headers).then((response) => response.json());
