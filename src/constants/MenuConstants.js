2a3d8a3b36633283f5f7d6b0ca38316111adb2d8
Ziqian Ge <ge.zi@husky.neu.edu>
2019-06-23 15:54:20 -0400

    used files deleted

--- src/constants/MenuConstants.js
+++
@@ -1,9 +0,0 @@
-export const ADD_FOOD_MENU = "ADD_FOOD_MENU";
-
-export const VIEW_MENU = "VIEW_MENU";
-
-export const VIEW_FOOD_MENU = "VIEW_FOOD_MENU";
-
-export const DELETE_FOOD_MENU = "DELETE_FOOD_MENU";
-
-export const UPDATE_FOOD_MENU = "UPDATE_FOOD_MENU";
